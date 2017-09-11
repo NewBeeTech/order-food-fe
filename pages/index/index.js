54c3524f480f57b7f8b9eca2fe0d1fb31fb2c3c9
wangxiaodan <wangxiaodan@hsohealth.com>
2017-09-11 15:31:58 +0800

    添加轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,34 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    toView: 'red',
+    scrollTop: 100
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  upper: function (e) {
+    console.log(e)
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   }
 })
