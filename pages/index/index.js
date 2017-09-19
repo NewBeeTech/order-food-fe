21125ff0b1d4c26b23bd34144b06b2829da321b4
wl <wl@WangLus-MacBook-Pro.local>
2017-09-19 11:58:49 +0800

    上拉加载组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,20 @@
 //index.js
+//引入上拉加载
+import { LoadMore } from '../../components/load-more/load-more';
+
 //获取应用实例
 var app = getApp()
 Page({
+  LoadMore,
+
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    total: 100, // 总数
+    current: 2, // 当前页
+    pageSize: 10, // 每页数据条数
+    list: [],
+    url: '' // url网络请求地址 如：`http://v.juhe.cn/weixin/query?key=f16af393a63364b729fd81ed9fdd4b7d&pno=${Number(this.data.current) + 1}&ps=${Number(this.data.pageSize)}`
   },
   //事件处理函数
   bindViewTap: function() {
@@ -13,14 +23,21 @@ Page({
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
+    console.log('onLoad');
+    var that = this;
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
-      })
-    })
+      });
+    });
+    // 页面加载时显示上拉状态：没有更多数据/需要上拉加载更多
+    that.LoadMore().show(that.data);
+  },
+  // 上拉加载更多
+  onReachBottom: function() {
+    var that = this;
+    that.LoadMore().loading(that.data);
   }
 })
